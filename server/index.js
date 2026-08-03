a6384ac39de1bea6ca08fbefe1792b2ebe5abe1e
shivani maddheshiya <shivanimaddheshiya26@gmail.com>
2026-08-03 21:24:24 +0530

    Fixed search bar and updated HomeHaven

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const fs = require('fs');
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
